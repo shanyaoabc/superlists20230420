1d119dec14950bce4baceb1859ed54315550f536
aoyufei <aoyufei@inke.cn>
2023-04-22 21:26:02 +0800

    eeeeee

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,12 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser = webdriver.Firefox()
 	def teardown(self):
 		self.browser.quit()
+
+	def check_for_fow_in_list_table(self, row_text):
+		table = self.browser.find_element(By.ID, 'id_list_table')
+		rows = table.find_elements(By.TAG_NAME, 'tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start(self):
 		# 待办事项应用，去看这个应用的首页
 		self.browser.get('http://localhost:8000')
@@ -33,11 +39,20 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 		table = self.browser.find_element(By.ID, 'id_list_table')
-		rows = table.find_elements(By.NAME, 'tr')
+		rows = table.find_elements(By.TAG_NAME, 'tr')
 		# self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), f"New to-do item did not appear in table. Contents were:\n{table.text}")
-		self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 		#页面又显示文本框，输入其他待办事项
 		#输入 “Use peacock feathers to make a fly"
+		inputbox = self.browser.find_element(By.ID, 'id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+		# 页面再次更新，她的清单中显示两个待办事项
+		self.check_for_fow_in_list_table('1: Buy peacock feathers')
+		self.check_for_fow_in_list_table('2: Use peacock feathers to make a fly')
+		# 想知道网站会记住她的清单
+
 		self.fail('Finished the test!')
 		# 页面再次更新，清单有两个待办事项
 
