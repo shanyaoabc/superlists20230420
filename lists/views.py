f7f496272f45162b46f16da4d815cd02c83cae7e
aoyufei <aoyufei@inke.cn>
2023-04-22 16:12:02 +0800

    template add pass

--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-	return HttpResponse('<html><title>To-Do lists</title></html>')
+	# render函数，Django会自动在素有应用目录中搜索名为templates的文件夹，然后根据模板的内容构建一个HttpRespouse对象
+	# 使用模版有时之一把python变量带入HTML文本
+	return render(request, 'home.html')
