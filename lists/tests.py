25b65f7fddc7c70c348291cca11ab254d5be7f4b
aoyufei <aoyufei@inke.cn>
2023-04-23 21:32:56 +0800

    Model for list Items and asscociated migration

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from lists.models import Item
 # from django.urls import resolve
 # from lists.views import home_page
 # from django.http import HttpRequest
@@ -6,6 +7,23 @@ from django.test import TestCase
 
 
 # Create your tests here.
+
+class ItemModelTest(TestCase):
+	"""docstring for lists.models"""
+	def test_saving_and_retrieving_items(self):
+		frist_item = Item()
+		frist_item.text = 'The first (ever) list item'
+		frist_item.save()
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+		frist_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(frist_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(second_saved_item.text, 'Item the second')
+
 class  HomePageTest(TestCase):
 	"""docstring for  SmokeTest"""
 	# def test_root_url_resolves(self):
@@ -24,4 +42,3 @@ class  HomePageTest(TestCase):
 		response = self.client.post('/', data={'item_text': 'A new list item'})
 		self.assertIn('A new list item', response.content.decode())
 		self.assertTemplateUsed(response, 'home.html')
-		
