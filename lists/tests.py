1a25456900f3bec11f0c69fce8b61f6615649b6b
aoyufei <aoyufei@inke.cn>
2023-04-24 16:03:52 +0800

    Redirect after POST, and show all items in templeate

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,6 @@
 from django.test import TestCase
 from lists.models import Item
-# from django.urls import resolve
-# from lists.views import home_page
-# from django.http import HttpRequest
-# from django.template.loader import render_to_string
+
 
 
 # Create your tests here.
@@ -24,21 +21,36 @@ class ItemModelTest(TestCase):
 		self.assertEqual(frist_saved_item.text, 'The first (ever) list item')
 		self.assertEqual(second_saved_item.text, 'Item the second')
 
+
 class  HomePageTest(TestCase):
 	"""docstring for  SmokeTest"""
-	# def test_root_url_resolves(self):
-		# found = resolve('/')
-		# self.assertEqual(found.func, home_page)
 	def test_use_home_template(self):
-		# request = HttpRequest() # 创建一个httprequest对象，用户在浏览器中请求网页时，Djiango看到的就是htpprequest对象
-		# response = home_page(request) # 把这个对象传给home-page视图，得到响应。响应对象是httpresponse对象
 		response = self.client.get('/') # 不在手动创建httprequest对象，不直接调用视图函数
-		# html = response.content.decode('utf-8') # 提取相应内容，得到响应原始字节，调用decode,把原始字节转换成字符串
-		# self.assertTrue(html.startswith('<html>')) 
-		# self.assertIn('<title>To-Do lists</title>', html) # 单元测试由功能测试驱动，而且更接近于真正的代码。编写单元测试时，按照程序员的方式思考。
-		# self.assertTrue(html.endswith('</html>'))
 		self.assertTemplateUsed(response, 'home.html') # assertTemplateUsed是TestCase类方法，检查相应使用的是哪个模版渲染的
+
 	def test_can_save_a_POST_request(self):
 		response = self.client.post('/', data={'item_text': 'A new list item'})
-		self.assertIn('A new list item', response.content.decode())
-		self.assertTemplateUsed(response, 'home.html')
+		# 检查是否把一个新item对象存入数据库
+		self.assertEqual(Item.objects.count(), 1)
+		news_item = Item.objects.first()
+		self.assertEqual(news_item.text, 'A new list item')
+
+	def test_redirects_after_POST(self):
+		response = self.client.post('/', data={'item_text':'A new list item'})
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+
+	def  test_only_saves_items_when_necessary(self):
+		self.client.get('/')
+		self.assertEqual(Item.objects.count(), 0)
+
+	def test_displays_all_list_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+		response = self.client.get('/')
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 2', response.content.decode())
+
+
+
+
--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from  lists.models import Item
 
 # Create your views here.
 def home_page(request):
-	# render函数，Django会自动在素有应用目录中搜索名为templates的文件夹，然后根据模板的内容构建一个HttpRespouse对象
-	# 使用模版有时之一把python变量带入HTML文本
-	return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', ''),})
+	if request.method == 'POST':
+		Item.objects.create(text=request.POST['item_text'])
+		return redirect('/')
+	items = Item.objects.all()
+	return render(request, 'home.html', {'items': items})
