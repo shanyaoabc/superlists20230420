b93ffdfe03817afa97333180149724db08c6a124
aoyufei <aoyufei@inke.cn>
2023-04-25 10:29:44 +0800

    对URL映射做了些改动，views.py看起来也精简多了，而且能保证应用还能像以前那样正常运行。我们的重构技术变得越来越好了！

--- lists/tests.py
+++ lists/tests.py
@@ -28,24 +28,6 @@ class  HomePageTest(TestCase):
 		response = self.client.get('/') # 不在手动创建httprequest对象，不直接调用视图函数
 		self.assertTemplateUsed(response, 'home.html') # assertTemplateUsed是TestCase类方法，检查相应使用的是哪个模版渲染的
 
-	def test_can_save_a_POST_request(self):
-		response = self.client.post('/', data={'item_text': 'A new list item'})
-		# 检查是否把一个新item对象存入数据库
-		self.assertEqual(Item.objects.count(), 1)
-		news_item = Item.objects.first()
-		self.assertEqual(news_item.text, 'A new list item')
-
-	def test_redirects_after_POST(self):
-		response = self.client.post('/', data={'item_text':'A new list item'})
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-	def  test_only_saves_items_when_necessary(self):
-		self.client.get('/')
-		self.assertEqual(Item.objects.count(), 0)
-
-
-
 class ListViewTest(TestCase):
 
 	def test_uses_list_template(self):
@@ -58,4 +40,19 @@ class ListViewTest(TestCase):
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 		self.assertContains(response, 'itemey 1')
 		self.assertContains(response, 'itemey 2')
+
+
+class NewListTest(TestCase):
+
+	def test_can_save_a_POST_request(self):
+		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+		# 检查是否把一个新item对象存入数据库
+		self.assertEqual(Item.objects.count(), 1)
+		news_item = Item.objects.first()
+		self.assertEqual(news_item.text, 'A new list item')
+
+	def test_redirects_after_POST(self):
+		response = self.client.post('/lists/new', data={'item_text':'A new list item'})
+		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+
 
