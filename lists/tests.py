6678bfb793d582bbeda1991e17e5adb59d30c30b
aoyufei <aoyufei@inke.cn>
2023-04-25 12:14:47 +0800

    测试的结果和修改前一样。现有功能没有破坏，在此基础上还修改了数据库。这一点令人欣喜！

--- lists/tests.py
+++ lists/tests.py
@@ -1,25 +1,35 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 
 
 
 # Create your tests here.
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 	"""docstring for lists.models"""
 	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+
 		frist_item = Item()
+
 		frist_item.text = 'The first (ever) list item'
+		frist_item.list = list_
 		frist_item.save()
 		second_item = Item()
 		second_item.text = 'Item the second'
+		second_item.list = list_
 		second_item.save()
+		save_list = List.objects.first()
+		self.assertEqual(save_list, list_)
 		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count(), 2)
 		frist_saved_item = saved_items[0]
 		second_saved_item = saved_items[1]
 		self.assertEqual(frist_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(frist_saved_item.list, list_)
 		self.assertEqual(second_saved_item.text, 'Item the second')
+		self.assertEqual(second_saved_item.list, list_)
 
 
 class  HomePageTest(TestCase):
@@ -35,8 +45,9 @@ class ListViewTest(TestCase):
 		self.assertTemplateUsed(response, 'list.html')
 
 	def test_displays_all_list_items(self):
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
+		list_ = List.objects.create()
+		Item.objects.create(text='itemey 1', list = list_)
+		Item.objects.create(text='itemey 2', list = list_)
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 		self.assertContains(response, 'itemey 1')
 		self.assertContains(response, 'itemey 2')
