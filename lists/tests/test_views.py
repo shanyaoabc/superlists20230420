be59de87e523479c21dd222df5c0dab76e214002
aoyufei <aoyufei@inke.cn>
2023-05-01 19:11:09 +0800

    拆分单元测试到两个文件中

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,32 +5,6 @@ from lists.models import Item, List
 
 # Create your tests here.
 
-class ListAndItemModelTest(TestCase):
-	"""docstring for lists.models"""
-	def test_saving_and_retrieving_items(self):
-		list_ = List()
-		list_.save()
-
-		frist_item = Item()
-
-		frist_item.text = 'The first (ever) list item'
-		frist_item.list = list_
-		frist_item.save()
-		second_item = Item()
-		second_item.text = 'Item the second'
-		second_item.list = list_
-		second_item.save()
-		save_list = List.objects.first()
-		self.assertEqual(save_list, list_)
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(), 2)
-		frist_saved_item = saved_items[0]
-		second_saved_item = saved_items[1]
-		self.assertEqual(frist_saved_item.text, 'The first (ever) list item')
-		self.assertEqual(frist_saved_item.list, list_)
-		self.assertEqual(second_saved_item.text, 'Item the second')
-		self.assertEqual(second_saved_item.list, list_)
-
 
 class  HomePageTest(TestCase):
 	"""docstring for  SmokeTest"""
