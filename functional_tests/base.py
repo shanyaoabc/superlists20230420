6436397470e5325798820b283f6ca3fe21aecf70
aoyufei <aoyufei@inke.cn>
2023-05-01 18:12:00 +0800

    修改功能测试，测试空内容回车，测试失败了，没有找到特定的CSS元素

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -18,6 +18,7 @@ class FunctionalTest(StaticLiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	
 	def wait_for_fow_in_list_table(self, row_text):
 		start_time = time.time()
 		while True:
@@ -30,3 +31,14 @@ class FunctionalTest(StaticLiveServerTestCase):
 				if time.time() - start_time > MAX_WAIT:
 					raise  e
 					time.sleep(0.5)
+
+	def wait_for(self, fn):
+		start_time = time.time()
+		while True:
+			try:
+				return fn()
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+					time.sleep(0.5)
+
