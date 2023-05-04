d79ee13c9f9448189af1b6a935b97bfecbe2e356
aoyufei <aoyufei@inke.cn>
2023-05-04 15:37:12 +0800

    Refactor list view to handle new item POSTs

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -39,6 +39,20 @@ class ListViewTest(TestCase):
 		response = self.client.get(f'/lists/{correct_list.id}/')
 		self.assertEqual(response.context['list'], correct_list)
 
+	def test_can_save_a_POST_request_to_an_existing_list(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+		self.client.post(f'/lists/{correct_list.id}/', data={'item_text': 'A new item for an existing list'})
+		self.assertEquals(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new item for an existing list')
+		self.assertEqual(new_item.list, correct_list)
+
+	def test_POST_redirects_to_list_view(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+		response = self.client.post(f'/lists/{correct_list.id}/', data={'item_text': 'A new item for an existing list'})
+		self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
 
 class NewListTest(TestCase):
@@ -55,20 +69,6 @@ class NewListTest(TestCase):
 		new_list = List.objects.first()
 		self.assertRedirects(response, f'/lists/{new_list.id}/')
 
-	def test_can_save_a_POST_request_to_an_existing_list(self):
-		other_list = List.objects.create()
-		correct_list = List.objects.create()
-		self.client.post(f'/lists/{correct_list.id}/add_item', data={'item_text': 'A new item for an existing list'})
-		self.assertEquals(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new item for an existing list')
-		self.assertEqual(new_item.list, correct_list)
-
-	def test_redirects_to_list_view(self):
-		other_list = List.objects.create()
-		correct_list = List.objects.create()
-		response = self.client.post(f'/lists/{correct_list.id}/add_item', data={'item_text': 'A new item for an existing list'})
-		self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
 	def test_validation_errors_are_sent_back_to_home_page_template(self):
 		response = self.client.post('/lists/new', data={'item_text': ''})
