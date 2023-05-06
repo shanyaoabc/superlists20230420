75cc41c357757594873369a7aba7f5a682660f3f
aoyufei <aoyufei@inke.cn>
2023-05-06 19:02:08 +0800

    use form in all views,back to working state

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -12,25 +12,26 @@ class ItemValidationTest(FunctionalTest):
 	    # 输入框中没输入内容，她就按下了回车键
 	    self.browser.get(self.live_server_url)
 	    self.get_item_input_box().send_keys(Keys.ENTER)
-	    # 首页刷新了，显示一个错误消息
-	    # 提示待办事项不能为空
-	    self.wait_for(lambda: self.assertEqual(
-	    	self.browser.find_element(By.CSS_SELECTOR, '.has-error').text,
-	    	"You cannot have an empty list item"
-	    	))
-	    # 她输入一些文字，然后再次提交，这次没问题了
+	    # 浏览器截获了请求
+	    # 清单页面不会加载
+	    self.wait_for(lambda: self.browser.find_element(By.CSS_SELECTOR, '#id_text:invalid'))
+	    # 她在待办事项中输入了一些文字
+	    # 错误消失了
 	    self.get_item_input_box().send_keys('Buy milk')
+	    self.wait_for(lambda: self.browser.find_element(By.CSS_SELECTOR, '#id_text:valid'))
+	    # 现在能提交了
 	    self.get_item_input_box().send_keys(Keys.ENTER)
 	    self.wait_for_fow_in_list_table('1: Buy milk')
 	    # 她有点儿调皮，又提交了一个空待办事项
 	    self.get_item_input_box().send_keys(Keys.ENTER)
-	    # 在清单页面她看到了一个类似的错误消息
-	    self.wait_for(lambda: self.assertEqual(self.browser.find_element(By.CSS_SELECTOR, '.has-error').text, "You cannot have an empty list item") )
-
-	    # 输入文字之后就没问题了
+	    # 浏览器这次不会放行
+	    self.wait_for_fow_in_list_table('1: Buy milk')
+	    self.wait_for(lambda: self.browser.find_element(By.CSS_SELECTOR, '#id_text:invalid'))
+	    # 输入一些文字后就能纠正这个错误
 	    self.get_item_input_box().send_keys('Make tea')
+	    self.wait_for(lambda: self.browser.find_element(By.CSS_SELECTOR, '#id_text:valid'))
 	    self.get_item_input_box().send_keys(Keys.ENTER)
 	    self.wait_for_fow_in_list_table('1: Buy milk')
 	    self.wait_for_fow_in_list_table('2: Make tea')
 
-	    self.fail('finish this test!')
+	    # self.fail('finish this test!')
--- lists/forms.py
+++ lists/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from lists.models import Item
 
 EMPTY_ITEM_ERROR = "You cannot have an empty list item"
-
 class ItemForm(forms.models.ModelForm):
+    
     class Meta:
         model = Item
         fields = ('text',)
@@ -12,9 +12,8 @@ class ItemForm(forms.models.ModelForm):
                 'placeholder': 'Enter a to-do item',
                 'class': 'form-control input-lg',
             }),
-        }
-        
-        errors_messages = {
+        }   
+        error_messages = {
         'text': {'required': EMPTY_ITEM_ERROR}
 
         }
