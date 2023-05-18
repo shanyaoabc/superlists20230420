0b6628d9d7b0568cd000f909151c88d286e84c7e
aoyufei <aoyufei@inke.cn>
2023-05-18 16:28:56 +0800

    16章js修改功能测试内容

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,6 +6,8 @@ from unittest import skip
 MAX_WAIT = 10
 
 class ItemValidationTest(FunctionalTest):
+	def get_error_element(self):
+		return self.browser.find_element(By.CSS_SELECTOR, '.has-error')
 
 	def test_cannot_add_empty_list_items(self):
 	    # 伊迪丝访问首页，不小心提交了一个空待办事项
@@ -44,5 +46,32 @@ class ItemValidationTest(FunctionalTest):
 		self.get_item_input_box().send_keys('Buy wellies')
 		self.get_item_input_box().send_keys(Keys.ENTER)
 		# 她看到一条有帮助的错误消息
-		self.wait_for(lambda: self.assertEqual(self.browser.find_element(By.CSS_SELECTOR, '.has-error').text, "You've already got this in your list"))
+		self.wait_for(lambda: self.assertEqual(self.get_error_element().text, "You've already got this in your list"))
+
+	def test_error_message_are_cleared_on_input(self):
+		# 伊迪斯新建一个清单，但方法不当，所以出现一个验证错误
+		self.browser.get(self.live_server_url)
+		self.get_item_input_box().send_keys('Banter too thick')
+		self.get_item_input_box().send_keys(Keys.ENTER)
+		self.wait_for_fow_in_list_table('1: Banter too thick')
+		self.get_item_input_box().send_keys('Banter too thick')
+		self.get_item_input_box().send_keys(Keys.ENTER)
+		self.wait_for(lambda: self.assertTrue(
+			self.get_error_element().is_displayed()
+			))
+		# 为了消除错误，她开始在输入框中输入内容
+		self.get_item_input_box().send_keys('a')
+		# 看到错误消息消失了，她很高兴
+		self.wait_for(lambda: self.assertFalse(
+			self.get_error_element().is_displayed()
+			))
+
+
+
+
+		
+
+
+
+
 
